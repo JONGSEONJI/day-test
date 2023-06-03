b44646a2f40ac1753588f37078c3a9539dab9753
jijong <jjs3787@gmail.com>
2023-06-03 18:35:15 +0900

    Hi World 추가

--- src/day01/HelloWolrd.java
+++ src/day01/HelloWolrd.java
@@ -3,5 +3,7 @@ package day01;
 public class HelloWolrd {
 	public static void main(String[] args) {
 		System.out.println("test");
+		System.out.println("Hi World");
+		System.out.println("Hi World");
 	}
 }
